2406b82824e2ab95536827db7da6306f719b6e80
shacharpooyae <shacharpooyae@gmail.com>
2014-04-13 18:09:30 +0300

    Verifier can now validate passwords. log in window + listers . 2 quesries for database needs to be implented

--- db2014_parser/src/bl/verifier.java
+++ db2014_parser/src/bl/verifier.java
@@ -8,5 +8,10 @@ public class verifier {
 			return false;
 		return true;
 	}
+	public static boolean verifyPass(String password){ //pasword is 1-6 characters only alpha numeric
 
+		if(!password.matches("[a-zA-Z0-9]{1,6}"))
+			return false;
+		return true;
+	}
 }
