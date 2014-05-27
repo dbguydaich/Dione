5977d02bfdf7610558ba19c5aa28a212ef85d408
guydaich <guydaich@gmail.com>
2014-05-27 11:39:44 +0300

    fix

--- db2014_parser/src/parser_entities/parser_tester.java
+++ db2014_parser/src/parser_entities/parser_tester.java
@@ -27,22 +27,13 @@ public class parser_tester {
 		/*peripherial data*/
 		try {
 			importer db_importer = new importer();
-			//db_importer.update_genres(imdb_parser.get_parser_genres());
-			//db_importer.update_languages(imdb_parser.get_parser_languages());
-			//db_importer.update_tags(imdb_parser.get_parser_tags());
-			//db_importer.update_actors_table(yago_parser.get_yag_actor_map());
-			//db_importer.update_directors_table(yago_parser.get_yag_director_map());
-			//db_importer.update_movies_genres_table(yago_parser.get_yag_movie_map());
-			//db_importer.update_movies_tags_table(yago_parser.get_yag_movie_map());
-			//db_importer.update_movie_actors_tabls(yago_parser.get_yag_movie_map());*/
-
 			/*new*/
-			//db_importer.update_objects_by_name_batch(imdb_parser.get_parser_languages(), importer.OBJECT_LANGUAGE);
-			//db_importer.update_objects_by_name_batch(imdb_parser.get_parser_genres(), importer.OBJECT_GENRE);
-			//db_importer.update_objects_by_name_batch(imdb_parser.get_parser_tags(), importer.OBJECT_TAG);
+			db_importer.update_objects_by_name_batch(imdb_parser.get_parser_languages(), importer.OBJECT_LANGUAGE);
+			db_importer.update_objects_by_name_batch(imdb_parser.get_parser_genres(), importer.OBJECT_GENRE);
+			db_importer.update_objects_by_name_batch(imdb_parser.get_parser_tags(), importer.OBJECT_TAG);
 			/*entities*/
-			//db_importer.update_person_entities_table(yago_parser.get_yag_actor_map(), importer.PERSON_TYPE_ACTOR);
-			//db_importer.update_person_entities_table(yago_parser.get_yag_director_map(), importer.PERSON_TYPE_DIRECTOR);
+			db_importer.update_person_entities_table(yago_parser.get_yag_actor_map(), importer.PERSON_TYPE_ACTOR);
+			db_importer.update_person_entities_table(yago_parser.get_yag_director_map(), importer.PERSON_TYPE_DIRECTOR);
 			db_importer.update_movies_table(yago_parser.get_yag_movie_map());
 			/*movie many to many*/
 			db_importer.update_movie_multiple_attributes(yago_parser.get_yag_movie_map(), importer.MOVIE_ATTRIBUTE_ACTORS);
