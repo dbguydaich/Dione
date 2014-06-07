c4888f337f21214c57b4e83cf40a0d407028e539
shaharavital <shahar7878@gmail.com>
2014-06-07 12:18:47 +0300

    all tabs win

--- db2014_parser/src/gui/all_tabs_window.java
+++ db2014_parser/src/gui/all_tabs_window.java
@@ -7,25 +7,20 @@ import java.util.List;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.DeviceData;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
-import org.eclipse.swt.widgets.Text;
-
 import parser_entities.light_entity_movie;
 import bl.user_logics;
 import config.config;
 
+
+/**
+ * All Tabs Window
+ * This is the main window
+ */
 public class all_tabs_window extends Shell {
 
 	config config = new config();
@@ -36,25 +31,22 @@ public class all_tabs_window extends Shell {
 	List<String> user_tags_string;
 
 	public all_tabs_window(final Display display) {
+	
 		super(display, SWT.SHELL_TRIM & (~SWT.RESIZE) & (~SWT.MAX));
-
 		this.setSize(window_width, window_height);
-
-		System.out.println("tabs win creating...");
-
 		String my_name = null;
+		
 		try {
-			 my_name = log_in_window.user.get_my_name();
+			 my_name = log_in_window.user.get_my_name(); /* get current logged in username */
 		} catch (SQLException e2) {
 			gui_utils.raise_sql_error_window(display);
 		}
 
-		this.setText("MovieBook - Logged in As: " +  my_name);
+		this.setText("MovieBook - Logged in As: " +  my_name); /* window title */
+		this.setLayout(new FillLayout()); /* window layout */
+		final TabFolder tab_folder = new TabFolder(this, SWT.NONE); /* the window consists of tabs */
 
-		this.setLayout(new FillLayout());
-		final TabFolder tab_folder = new TabFolder(this, SWT.NONE);
-
-		// tab1
+		//tab1
 		final TabItem tab1 = new TabItem(tab_folder, SWT.NONE);
 		tab1.setText("Overview");
 
@@ -78,113 +70,99 @@ public class all_tabs_window extends Shell {
 		
 		
 		
-		//////getting data for tab 1/////
+		/* getting data from the db to the first tab */
 		Thread t = new Thread(new Runnable() {
-
 			public void run() {
-
-				// ////new/////
-
-				List<String> movies_my_taste = null;
 				try {
-					movies_my_taste_entity = user_logics
-							.get_user_recommended_movies(log_in_window.user
-									.get_current_user_id());
-					movies_my_taste = gui_utils
-							.convert_movies_entity_to_string(movies_my_taste_entity);
+					/* get user recommended movies */
+					movies_my_taste_entity = 
+							user_logics.get_user_recommended_movies(log_in_window.user.get_current_user_id());
 				} catch (SQLException e1) {
-					movies_my_taste = new ArrayList<String>();
-					// TODO Auto-generated catch block
 					gui_utils.raise_sql_error_window(display);
 
 					e1.printStackTrace();
 				}
 
-				// taste_tags
-				//
-
 				try {
+					/* get user preferred tags */
 					user_tags_string = log_in_window.user
 							.get_user_popular_tags();
 				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
 					user_tags_string = new ArrayList<String>();
 					gui_utils.raise_sql_error_window(display);
 					e1.printStackTrace();
-				} // to be used when function exists
-
-				// user recent activities labels
+				} 
 
 				try {
+					/* recent user activities */
 					user_activities_strings = user_logics.get_user_recent_string_activities(
 							log_in_window.user.get_current_user_id(), 6);
 
 				} catch (SQLException e1) {
 					user_activities_strings = new ArrayList<String>();
 					gui_utils.raise_sql_error_window(display);
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-
+				
+				/* updating the gui thread  */
 				display.asyncExec(new Runnable() {
-
 					public void run() {
-
+						/* opening the overview tab */
 						overview_tab my_overview_tab = new overview_tab(
 								display, tab_folder, SWT.NONE,
 								movies_my_taste_entity, user_tags_string,
 								user_activities_strings);
 						tab1.setControl(my_overview_tab);
-
 					}
-
 				});
-
 			}
-
 		});
 
-		gui_utils.executor.execute(t);
+		
+		gui_utils.executor.execute(t); /* executer will run the thread */
 
+		/* search movie tab */
 		search_movie_tab my_search_movie_tab = new search_movie_tab(display,
 				tab_folder, SWT.NONE);
 		tab2.setControl(my_search_movie_tab);
 
+		/* social tab */
 		social_tab my_social_tab = new social_tab(display, tab_folder, SWT.NONE);
 		tab3.setControl(my_social_tab);
 
+		/* recommendation tab */
 		recommendation_tab my_recommendation_tab = new recommendation_tab(
 				display, tab_folder, SWT.NONE);
 		tab4.setControl(my_recommendation_tab);
 
+		/* setting tab */
 		settings_tab my_settings_tab = new settings_tab(display, tab_folder,
 				SWT.NONE);
 		tab5.setControl(my_settings_tab);
 
+		/* Disposal Listener */
 		this.addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
-				for (movie_details_window win : gui_utils.movie_windows) {
+				for (movie_details_window win : gui_utils.movie_windows) { /* close all opened movie windows */
 					if (!win.isDisposed())
 						win.dispose();
 				}
-
-				if (gui_utils.pref_win != null)
-					if (!gui_utils.pref_win.isDisposed()) {
+				if (gui_utils.pref_win != null) /* if pref_win exists */
+					if (!gui_utils.pref_win.isDisposed()) { /* and opened */
 						gui_utils.EXIT_ON_LOGIN = false;
 						gui_utils.pref_win.dispose();
 					}
-				if (gui_utils.EXIT_ON_LOGIN == true) {
+				if (gui_utils.EXIT_ON_LOGIN == true) { /* EXIT */
 					display.dispose();
-					// shachar: app is exiting here
 					gui_utils.exist_threads();
 				}
-
 				else
 					gui_utils.EXIT_ON_LOGIN = true;
 			}
 		});
 	}
 
+	
 	protected void checkSubclass() {
 	}
 
